9cc28784c2e28b917d3c34ea6c0e6d45063a897a
nouhaila <nouhailaboutrique@gmail.gom>
2025-11-04 12:17:59 +0100

    structur initial de projet

---
+++ config/connexion.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'todolist');
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
---
+++ config/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h2>To-Do List Project</h2>";
+echo "<p>Project setup successful </p>";
+?>
+
